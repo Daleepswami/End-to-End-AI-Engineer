d051c31e509691379ce83c8cb58a32f9c9f2f6a5
Daleepswami <daleepswami22170@gmail.com>
2026-02-03 23:44:15 +0530

    Add files via upload

---
+++ SQL/01_Learning/DQL(Data Query Language)/SELECT.sql
@@ -0,0 +1,17 @@
+use sakila;
+
+-- SELECT statement fetches required columns and rows from a database table.
+
+-- all column fatch from tables
+select * from actor;
+
+select * from  city;
+
+
+-- fatch the specific columns in table
+select city ,country_id from  city;
+
+select first_name,last_name from actor;
+
+
+
