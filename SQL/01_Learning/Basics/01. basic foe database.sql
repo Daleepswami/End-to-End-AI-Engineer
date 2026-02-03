c8a05aea1f90c979d31c7322ca54413c5612efca
Daleepswami <daleepswami22170@gmail.com>
2026-02-03 17:29:20 +0530

    Delete SQL/01_Learning/Basics/01. basic foe database.sql

--- SQL/01_Learning/Basics/01. basic foe database.sql
+++
@@ -1,9 +0,0 @@
--- how to see the all database 
-show databases;
--- how to create new databases 
-create database regex_2;
--- use the database 
--- syntax => use database_name
-use regex_2;
- -- delete/remove /drop the database
- drop database regex_2;
