7255dfda5e8fd6013509f7a745f6f5c8b5194185
Daleepswami <daleepswami22170@gmail.com>
2026-02-18 14:13:15 +0530

    Update 01.Module.py

--- python/01_Learning/01_Foundation & Basic /01_Python_syntax_basic /01.Module.py
+++ python/01_Learning/01_Foundation & Basic /01_Python_syntax_basic /01.Module.py
@@ -1,1 +1,20 @@
+Python Module::=>> 
+    A Python module is a .py file containing reusable code like functions, classes, and variables.
+    It organizes code, avoids repetition, and keeps programs modular. 
+    Use built-in modules (math, random), create custom ones, or install third-party via pip.
+module Types:
+    Built-in: import math
+    User-defined: Create your own file
+
+01. Built-in:=>
+       import math
+       import mymod
+       print(mymod.greet("Student"))  # Hello Student!
+       print(mymod.PI)               # 3.14
+02.  User-defined: Create your own file:=>>
+       Create mymod.py:
+               def greet(name):
+                  return f"Hello {name}!"
+                    PI = 3.14
+
 
